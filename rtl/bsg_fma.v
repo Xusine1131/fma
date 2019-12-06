48ab26d0ce4d1868167c6a799962c6498134043d
Shanqing Lin <sqlin16@fudan.edu.cn>
2019-12-06 20:11:39 +0800

    remove unnecessary logic

--- rtl/bsg_fma.v
+++ rtl/bsg_fma.v
@@ -13,18 +13,7 @@ module bsg_fma #(
 localparam mul_width_lp = (sig_p + 1);
 
 wire [sig_p*2+1:0] res_part = opA_i[sig_p:0] * opB_i[sig_p:0];
-wire [exp_p-1:0] aux_part;
-
-bsg_fma_aux_adder #(
-    .e_p(exp_p)  
-) aux_adder (
-    .a_l_i(opA_i[exp_p-1:0])
-    ,.a_h_i(opA_i[width_lp-1:sig_p+1])
-    ,.b_l_i(opB_i[exp_p-1:0])
-    ,.b_h_i(opB_i[width_lp-1:sig_p+1])
-
-    ,.mod_o(aux_part)
-);
+wire [exp_p-1:0] aux_part = opA_i[exp_p-1:0] * opB_i[width_lp-1:sig_p+1] + opA_i[width_lp-1:sig_p+1] * opB_i[exp_p-1:0];
 
 assign mul_o = {res_part[width_lp-1:sig_p+1] + aux_part,res_part[sig_p:0]};
 
